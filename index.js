337a3cea3cad61fbe54269c3183de0ad9faa18f0
CarlavierVN <namto10042004@gmail.com>
2024-11-02 14:49:40 +0700

    modify index

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const app = require('express')()
-const PORT = 4000
+const app = require('express')();
+const PORT = 4000;
 
 app.listen(PORT, () => {
     console.log(`listening on http://localhost:${PORT}`);
-})
+});
 
 app.get("/test", (req, res) => {
     const { a, b } = req.query;
     if (!a || !b) res.status(400).send({ message: "a, b required" })
     res.status(200).send({ total: Number(a) + Number(b) })
-})
+});
+
+module.exports = app;
